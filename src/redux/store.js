0899650ed267a3a8c26e5b951682048af27949d5
Vlad Bond <foolowin@gmail.com>
2021-02-05 13:51:35 +0300

    tbc

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,9 @@ import rootReducer from './reducers';
 import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
-const store = createStore(rootReducer, compose(
-    applyMiddleware(
-        thunk,
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 
 export default store;
