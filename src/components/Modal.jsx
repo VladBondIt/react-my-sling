fefab18b24f05bace528e49df151c5ce09365662
Vlad Bond <foolowin@gmail.com>
2021-02-19 16:52:23 +0300

    fx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import CartModal from './CartModal';
 import OfferCallModal from './OfferCallModal';
 import { useSelector, useDispatch } from 'react-redux';
