11477832b43f200245358fa4c04d37e25b740df3
Vlad Bond <foolowin@gmail.com>
2021-02-24 13:11:20 +0300

    try fullstack

---
+++ server/modals/models.js
@@ -0,0 +1,26 @@
+const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define("user", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING, defaultValue: "USER" },
+})
+
+const Basket = sequelize.define("basket", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+})
+
+const BasketItem = sequelize.define("basket_item", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+})
+
+const Item = sequelize.define("item", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+    price: { type: DataTypes.INTEGER, allowNull: false },
+    oldprice: { type: DataTypes.INTEGER, allowNull: false },
+    rating: { type: DataTypes.INTEGER, defaultValue: 0 },
+    img: { type: DataTypes.STRING, allowNull: false }
+})
