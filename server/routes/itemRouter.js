e35f026ff9e26d8a1a8c51e195708c38c14adeae
Vlad Bond <foolowin@gmail.com>
2021-02-25 16:07:42 +0300

    auth jwt done

--- server/routes/itemRouter.js
+++ server/routes/itemRouter.js
@@ -1,10 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const itemController = require('../controllers/itemController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
 
-
-router.post('/', itemController.create)
+router.post('/', checkRole('ADMIN'), itemController.create)
 router.get('/', itemController.getAll)
 router.get('/:id', itemController.getOne)
 
